2906917435f7413d82fadaabc5a39da2a05a82d8
PetrPoling <46245539+PetrPoling@users.noreply.github.com>
2023-03-16 19:17:25 +0100

    Update RMS.cs

--- RMS_Father/RMS.cs
+++ RMS_Father/RMS.cs
@@ -1,11 +1,51 @@
 using System;
-namespace Managment;
 
-public class RMS
+namespace Rsm
 {
-    public static void Main()
+    public class Rms
     {
-        Console.WriteLine("Hello World!");
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please Input Ingredient_Name:");
+            Console.ReadLine();
+        }
+
+        public struct Ingredient
+        {
+            public string Name;
+            public decimal Price;
+
+        }
+
+        public struct Dish
+        {
+            public string Name;
+            public decimal Price;
+            public List<Ingredient> Ingredients;
+        }
+
+        public struct Order
+        {
+            public List<Dish> Dishes;
+            public decimal TotalPrice;
+        }
+
+        public struct Employee
+        {
+            public string Name;
+            public string Position;
+        }
+
+        public struct Table
+        {
+            public int Number;
+            public int Seats;
+        }
+
+        public struct Customer
+        {
+            public string Name;
+            public int PhoneNumber;
+        }
     }
-   
 }
